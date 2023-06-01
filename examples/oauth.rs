d116c4a98d5ce7e2ef67ce2b3a21713a21054b0a
gngpp <gngppz@gmail.com>
2023-06-01 05:21:28 +0000

    Complete the Token authentication store

--- examples/oauth.rs
+++ examples/oauth.rs
@@ -1,19 +1,21 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let store = openai::token::FileStore::default();
     let mut auth = openai::oauth::OpenOAuth0Builder::builder()
-        .email("opengpt@gmail.com".to_string())
-        .password("gngpp".to_string())
+        .email("gngppx@gmail.com".to_string())
+        .password("Fengge123???".to_string())
         .cache(true)
         .cookie_store(true)
-        .token_store(openai::token::Policy::file_store())
+        .token_store(store)
         .client_timeout(std::time::Duration::from_secs(20))
         .build();
-    let token = auth.authenticate().await?;
-    println!("Token: {}", token);
-    println!("Profile: {:#?}", auth.get_user_info()?);
-    tokio::time::sleep(std::time::Duration::from_secs(4)).await;
-    auth.do_refresh_token().await?;
-    tokio::time::sleep(std::time::Duration::from_secs(4)).await;
-    auth.do_revoke_token().await?;
+    let token = auth.do_get_access_token().await?;
+    println!("AccessToken: {}", token.access_token());
+    println!("RefreshToken: {}", token.refresh_token());
+    println!("Profile: {:#?}", token.profile());
+    // tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    // auth.do_refresh_token().await?;
+    // tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    // auth.do_revoke_token().await?;
     Ok(())
 }
